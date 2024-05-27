ee7dfb255af8bb6e90ee4aab9f737437ea2745de
CrisOdonel <162633626+CrisOdonel@users.noreply.github.com>
2024-05-27 13:51:00 -0500

    Update ItemList.java

--- ItemList.java
+++ ItemList.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
-public interface ItemList {
+public interface ItemList{
     public void AddItem();
     public void RemoveItem();
     public void EditItem();
+    //Sort ?
 }
