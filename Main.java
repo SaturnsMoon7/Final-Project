bb27ada930d83051c9553c7e174fbb5a1f557667
SaturnsMoon7 <161888542+SaturnsMoon7@users.noreply.github.com>
2024-05-28 15:13:23 -0500

    Fujnctiosn in

--- Main.java
+++ Main.java
@@ -1,8 +1,47 @@
+import java.util.ArrayList;
+
 class Main
 {
     public static void main(String[] args) 
     {
         ItemList items = new ItemList();
         items.run();
+
+        System.out.println("Welcome to the pantry");
+        
+
+    }
+
+    public ArrayList<Meal> initializeMealList()
+    {
+        ArrayList<Meal> newMealList = new ArrayList<Meal>();
+        boolean looping = true;
+        while (looping) 
+        {
+
+        }
+        return newMealList;
+    }
+
+    public int getUserInt(int min, int max)
+    {
+        int input = 0;
+        boolean looping = true;
+        while (looping) 
+        {
+            looping = false;
+        }   
+        return input;
+    }
+
+    public String getUserStr()
+    {
+        String input = "";
+        boolean looping = true;
+        while (looping) 
+        {
+            looping = false;
+        }   
+        return input;
     }
 }
