4579b6f88ad496c2440f9f2f6a264ea3252bedae
SaturnsMoon7 <161888542+SaturnsMoon7@users.noreply.github.com>
2024-05-29 13:25:06 -0500

    main update May 29

--- Main.java
+++ Main.java
@@ -4,12 +4,9 @@ class Main
 {
     public static void main(String[] args) 
     {
-        ItemList items = new ItemList();
-        items.run();
-
         System.out.println("Welcome to the pantry");
+        System.out.println("Begin by filling your pantry with avaiable ingredients!");
         
-
     }
 
     public ArrayList<Meal> initializeMealList()
@@ -18,18 +15,62 @@ class Main
         boolean looping = true;
         while (looping) 
         {
+            System.out.println("Type the name of the meal.");
+            System.out.println("Type 'done' if you are finished");
+
             String name = getUserStr();
-            IngredientList ingredients = IngredientList();
-            
+            if (name.toLowerCase() == "done") {
+                looping = false;
+                break;
+            }
+
+            IngredientList ingredients = initializeIngredientList();
+            Meal newMeal = new Meal(name, ingredients);
+            MealList.AddItem(newMeal);
         }
         return newMealList;
     }
 
-    public ArrayList<Ingredient> initializeIngredientList();
+    public IngredientList initializeIngredientList()
+    {
+        IngredientList newIngredients = new IngredientList();
+        boolean looping = true;
+        while (looping)
+        {
+            System.out.println("Type the name of the ingredient");
+            System.out.println("Type 'done' if you are finished");
 
-    public int getUserInt(int min, int max)
+            String name = getUserStr();
+            if (name.toLowerCase() == "done") {
+                looping = false;
+                break;
+            }
+            
+            System.out.println("Type the amount");
+            float amount = getUserFloat();
+        
+            Quantities quantity = new Quantities(amount, name);
+
+            Ingredient newIngredient = new Ingredient(name, quantity);
+            newIngredients.AddItem(newIngredient);
+        }
+        return newIngredients;
+    }
+
+    public int getUserInt()
     {
         int input = 0;
+        boolean looping = true;
+        while (looping) 
+        {
+            looping = false;
+        }   
+        return input;
+    }
+
+    public float getUserFloat()
+    {
+        float input = 0;
         boolean looping = true;
         while (looping) 
         {
