7b4dcb3e95e026aefaea06f6d36eedc6531371dd
Willchel <willchelc@live.wsd1.org>
2024-05-29 13:58:32 -0500

    w changes

--- MealList.java
+++ MealList.java
@@ -1,15 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class MealList extends Meal
 {
     private Scanner in;
     private String mealName;
     private Quantities amount;
-
-    private MealList()
-    {
-        meal = new MealList();
-        in = new Scanner(System.in);
-    }
 
     //This will add mealName to the ingredient list.t
     public MealList (String mealName, String name, IngredientList ingredients , int minutes, Quantities amount)
@@ -30,10 +25,25 @@ public class MealList extends Meal
         System.out.println(mealName);
     }
 
-    public void addmeal(String mealName, String name, IngredientList ingredients , int minutes, Quantities amount)
+    public void addmeal(String addMealname)
     {
         System.out.println("Please write the Meal name");
+        String mealName  = in.nextLine();
+        this.mealName.add(addMealname);
+    }
+
+    public void removemeal(String removeMealName)
+    {
+        System.out.println("Please write the name of the meal that you'd like to remove");
+        String mealName = in.nextLine();
+        this.mealName.remove(removeMealName);
         
-        
+    }
+
+    public void modifyMeal(String modifyMealName)
+    {
+        System.out.println("Please write down the name of the Meal that you'd like to modify");
+        String mealName = in.nextLine();
+        this.mealName.modify(modifyMealName);
     }
 }
