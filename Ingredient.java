82b6b4573427ddc876fd89efd316cabc5318d0c4
Willchel <willchelc@live.wsd1.org>
2024-05-31 13:42:25 -0500

    w changes

--- Ingredient.java
+++ Ingredient.java
@@ -1,28 +1,37 @@
-public class Ingredient{
+public class Ingredient
+{
     private String name;
     private Quantities amount;
 
-    Ingredient(String name, Quantities amount){
+    Ingredient(String name, Quantities amount)
+    {
         this.name = name;
         this.amount = amount;
     }
 
-    public void changeName(String newName){
+    public void changeName(String newName)
+    {
         this.name = newName;
     }
 
-    public void changeAmount(Quantities newAmount){
+    public void changeAmount(Quantities newAmount)
+    {
         this.amount = newAmount;
     }
 
-    public String getName(){
+    public String getName()
+    {
         return this.name;
     }
 
-    // Output:
-    // You can change it anyway you want Willchel gl
     public void displayIngredient()
     {
         System.out.println("Name: " + name + "\nAmount" + amount.toString());
+    }
+
+    public void add() 
+    {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
     }
 }
--- MealList.java
+++ MealList.java
@@ -1,13 +1,58 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class MealList extends ArrayList<Meal>
+public class MealList extends Meal
 {
-    public void EditName(int index, String newName)
-    { this.get(index).EditName(newName); }
+    private Scanner in;
+    private String mealName;
+    private Quantities amount;
 
-    public void DisplayList() 
+    List<String> mealList = new ArrayList<>();
+    //This will add mealName to the ingredient list.t
+    public MealList (String mealName, String name, IngredientList ingredients , int minutes, Quantities amount)
     {
-        for (int i = 0; i < this.size(); i++)
-        { System.out.println(this.get(i).getName()); }
+        super (name, ingredients, minutes);
+        this.amount = amount;
+        this.mealName = mealName;
     }
+
+    public String getMealName()
+    {
+        return this.mealName;
+    }
+
+    //This should get the name of the meal and put the ingredient together for the meal.
+    public void display()
+    {
+        System.out.println(mealName);
+    }
+
+    public void addmeal(String addMealname)
+    {
+        System.out.println("Please write the Meal name");
+        String mealName  = in.nextLine();
+        mealList.add(addMealname);
+    }
+
+    public void removemeal(String removeMealName)
+    {
+        System.out.println("Please write the name of the meal that you'd like to remove");
+        String mealName = in.nextLine();
+        mealList.remove(removeMealName);
+        
+    }
+
+    public void EditMealList(String newMealName)
+    {
+        this.mealName = newMealName;
+    }
+
+    public String setMealName()
+    {
+        return mealName;
+    }
+
+
+
 }
