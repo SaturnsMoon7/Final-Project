00bd815968882e8f1e2e4a8452d8141116f39f17
SaturnsMoon7 <161888542+SaturnsMoon7@users.noreply.github.com>
2024-05-31 13:50:36 -0500

    New MealList

--- MealList.java
+++ MealList.java
@@ -1,58 +1,10 @@
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
-public class MealList extends Meal
+public class MealList extends ArrayList<Meal>
 {
-    private Scanner in;
-    private String mealName;
-    private Quantities amount;
-
-    List<String> mealList = new ArrayList<>();
-    //This will add mealName to the ingredient list.t
-    public MealList (String mealName, String name, IngredientList ingredients , int minutes, Quantities amount)
+    public void Display()
     {
-        super (name, ingredients, minutes);
-        this.amount = amount;
-        this.mealName = mealName;
+        for (int i = 0; i < this.size(); i++)
+        { System.out.println(this.get(i).getName()); }
     }
-
-    public String getMealName()
-    {
-        return this.mealName;
-    }
-
-    //This should get the name of the meal and put the ingredient together for the meal.
-    public void display()
-    {
-        System.out.println(mealName);
-    }
-
-    public void addmeal(String addMealname)
-    {
-        System.out.println("Please write the Meal name");
-        String mealName  = in.nextLine();
-        mealList.add(addMealname);
-    }
-
-    public void removemeal(String removeMealName)
-    {
-        System.out.println("Please write the name of the meal that you'd like to remove");
-        String mealName = in.nextLine();
-        mealList.remove(removeMealName);
-        
-    }
-
-    public void EditMealList(String newMealName)
-    {
-        this.mealName = newMealName;
-    }
-
-    public String setMealName()
-    {
-        return mealName;
-    }
-
-
-
 }
