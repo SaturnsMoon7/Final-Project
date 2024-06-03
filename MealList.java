89340eaa39895516b75b94df121aa0708a8dc87d
SaturnsMoon7 <161888542+SaturnsMoon7@users.noreply.github.com>
2024-06-03 14:13:34 -0500

    meallist update

--- MealList.java
+++ MealList.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 
 public class MealList extends ArrayList<Meal>
 {
+    public void editMeal() {}
+    
+    public void sort() {}
+
     public void displayMeals()
     {
         for (int i = 0; i < this.size(); i++)
